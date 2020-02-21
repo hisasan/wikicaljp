bf9c5258c5a424ccbf21e757f817976ea831b748
Hisanobu Doi <hisasan@gmail.com>
2020-02-21 19:37:34 +0900

    lodashを使うように変更。

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
-const { JSDOM } = require('jsdom');
+const {JSDOM} = require('jsdom');
+const _ = require('lodash');
 
 // 指定項目の<li>エレメントを取得
 // 以下のような構造になっているので、<li>部分を全て取得する
@@ -21,16 +22,16 @@ function getSpecLI(dom, title) {
         throw Error('You must be call getContents() before calling this function.');
     }
     let elem = dom.window.document.getElementById(title);
-    elem = elem && elem.parentElement;
-    while (elem && elem.tagName != 'UL') {
+    elem = _.get(elem, 'parentElement', null);
+    while (_.isElement(elem) && elem.tagName != 'UL') {
         elem = elem.nextElementSibling;
     }
-    if (!elem) {
+    if (!_.isElement(elem)) {
         throw Error(`Specified title "${title}" element was not found`);
     }
     let lilist = [];
-    while (elem && elem.tagName != 'H2') {
-        lilist = lilist.concat(Array.from(elem && elem.querySelectorAll('li') || []));
+    while (_.isElement(elem) && elem.tagName != 'H2') {
+        lilist = lilist.concat(Array.from(elem.querySelectorAll('li') || []));
         elem = elem.nextElementSibling;
     }
     return lilist;
@@ -43,9 +44,8 @@ function getSpecLI(dom, title) {
 //  '植樹節（中国語版）（ 中国・ 中華民国）'
 function matchSP(s) {
     const pat1 = s.match(/[（(]\s(.+)[）)]/);
-    const pat2 = s.match(/[（(]\s([^）)]+)[）)]/);
     if (pat1 && pat1[1].match(/[）)].*[（(]/)) {
-        return pat2;
+        return s.match(/[（(]\s([^）)]+)[）)]/);
     }
     return pat1;
 }
@@ -59,44 +59,42 @@ function wikicaljp() {
 
 // 取得したページの記念日・年中行事一覧をオブジェクトで取得
 wikicaljp.prototype.getAnniversary = function() {
-    // 記念日・年中行事の<li>エレメントを取得
-    const lilist = getSpecLI(this.dom, '記念日・年中行事');
-    let list = [];
-    for (let i = 0; i < lilist.length; i++) {
+    // 記念日・年中行事の<li>エレメントを取得して返す
+    return getSpecLI(this.dom, '記念日・年中行事').reduce((list, value) => {
         // HTMLタグを削除して(XXX版)とか、[n]とかの注記を削除
-        const name = lilist[i].textContent.replace(/<("[^"]*"|'[^']*'|[^'">])*>|&nbsp;/g, '').replace(/（.+版）|\[.+\]/g, '').split(/\n|※/)[0].trim();
+        const name = value.textContent.replace(/<("[^"]*"|'[^']*'|[^'">])*>|&nbsp;/g, '').replace(/（.+版）|\[.+\]/g, '').split(/\n|※/)[0].trim();
         // XXの日（補足）  のような形式を  補足のXXの日  のように変換する
         const pat = matchSP(name);
-        if (pat && pat.length > 1) {
+        if (_.size(pat) > 1) {
             const item = name.slice(0, pat.index).trim();
             list.push({name:item, area:pat[1].trim()});
         } else {
             list.push({name});
         }
-    }
-    return list;
+        return list;
+    }, []);
 }
 
 // 取得したページの誕生日一覧をオブジェクトで取得
 wikicaljp.prototype.getBirthday = function() {
     // 誕生日の<li>エレメントを取得
-    const lilist = getSpecLI(this.dom, '誕生日');
-    let list = [];
-    for (let i = 0; i < lilist.length; i++) {
+    return getSpecLI(this.dom, '誕生日').reduce((list, value) => {
         // HTMLタグを削除して、"年 - 名前以降"を分割
-        const line = lilist[i].textContent.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, '').split('-');
+        const line = value.textContent.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, '').split('-');
         const year = line.shift().trim();
-        if (line.length > 0) {
+        if (_.size(line)) {
             // 最後の(+XXXX年)を削除して、”名前、職業"を分割
             const item = line.join('-').split(/\n|※/)[0].replace(/[（(][＋\+].+[）)]/g, '').split('、');
-            const obj  = {name:item.shift().trim(), year};
-            if (item.length > 0) {
-                obj.title = item.join('、').trim();
-            }
-            list.push(obj);
+            const name = item.shift().trim();
+            const title = item.join('、').trim();
+            list.push({
+                name,
+                ... _.size(title) ? {title} : {},
+                year
+            });
         }
-    }
-    return list;
+        return list;
+    }, []);
 }
 
 // ウィキペディアの指定月日のページを取得
